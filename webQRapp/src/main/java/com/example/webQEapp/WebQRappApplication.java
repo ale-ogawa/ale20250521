2ed7cb61cfef291385e5868084d67fcb68181442
Ryoichi Ogawa <r.ogawa@sn-kikaku.co.jp>
2025-11-05 11:32:43 +0900

    mainブランチのサンプル

---
+++ webQRapp/src/main/java/com/example/webQEapp/WebQRappApplication.java
@@ -0,0 +1,13 @@
+package com.example.webQEapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQRappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebQRappApplication.class, args);
+	}
+
+}
---
+++ webQRapp/src/test/java/com/example/webQEapp/WebQRappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webQEapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQRappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
